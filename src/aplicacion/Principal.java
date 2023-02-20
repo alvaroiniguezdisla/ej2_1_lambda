1c9be5d880066a934096429752f84b25b966c669
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 16:51:08 +0100

    ej1 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_1_lambda;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(ej2_1_lambda.suma(n));
+    }
+
 }
--- src/dominio/ej2_1_lambda.java
+++ src/dominio/ej2_1_lambda.java
@@ -1,2 +1,10 @@
-package dominio;public class ej2_1_lambda {
+package dominio;
+
+import java.util.stream.Stream;
+
+public class ej2_1_lambda {
+
+    public static int suma(int n) {
+        return Stream.iterate(0, i -> i + 1).limit(n+1).reduce(0, (a, b) -> a + b);
+    }
 }
